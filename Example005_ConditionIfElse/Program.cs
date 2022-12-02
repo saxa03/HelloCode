89b69bc6a2ce7f378251d055acf06b9789d6abd2
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-02 14:32:57 +0800

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша");//tolower() позволяет ввести имя "маша" с заглавной или строчной буквы, Маша, маШа и т.д.
+{
+    Console.WriteLine("Ура!Это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
